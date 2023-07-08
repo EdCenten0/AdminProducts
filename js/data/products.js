38766a6c648299b43804e6aeaf7ab6af53222d71
Ed Centeno <cchavarriacenteno8@gmail.com>
2023-07-07 23:11:44 -0600

    Functions to get products from the api

---
+++ js/data/products.js
@@ -0,0 +1,24 @@
+const API = 'https://api.escuelajs.co/api/v1';
+
+function fetchData(urlAPI){
+    return window.fetch(urlAPI)
+}
+
+export async function getProducts(){
+    return new Promise((resolve, reject) => {
+        fetchData(`${API}/products`)
+            .then((response) => response.json())
+            .then((products) => resolve(products))
+            .catch((err) => reject(err))
+    })
+}
+
+export async function getOneProduct(productId){
+    return new Promise((resolve, reject) => {
+        fetchData(`${API}/products/${productId}`)
+        .then((response) => response.json())
+        .then((product) => resolve(product))
+        .catch((err) => reject(err));
+    })
+}
+

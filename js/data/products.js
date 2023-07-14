9c1ae05c11fdca01aa69981ae440e5392473328e
Ed Centeno <cchavarriacenteno8@gmail.com>
2023-07-14 16:18:26 -0600

    Js products formated

--- js/data/products.js
+++ js/data/products.js
@@ -1,24 +1,23 @@
-const API = 'https://api.escuelajs.co/api/v1';
+const API = "https://api.escuelajs.co/api/v1";
 
-function fetchData(urlAPI){
-    return window.fetch(urlAPI)
+function fetchData(urlAPI) {
+  return window.fetch(urlAPI);
 }
 
-export async function getProducts(){
-    return new Promise((resolve, reject) => {
-        fetchData(`${API}/products`)
-            .then((response) => response.json())
-            .then((products) => resolve(products))
-            .catch((err) => reject(err))
-    })
+export async function getProducts() {
+  return new Promise((resolve, reject) => {
+    fetchData(`${API}/products`)
+      .then((response) => response.json())
+      .then((products) => resolve(products))
+      .catch((err) => reject(err));
+  });
 }
 
-export async function getOneProduct(productId){
-    return new Promise((resolve, reject) => {
-        fetchData(`${API}/products/${productId}`)
-        .then((response) => response.json())
-        .then((product) => resolve(product))
-        .catch((err) => reject(err));
-    })
+export async function getOneProduct(productId) {
+  return new Promise((resolve, reject) => {
+    fetchData(`${API}/products/${productId}`)
+      .then((response) => response.json())
+      .then((product) => resolve(product))
+      .catch((err) => reject(err));
+  });
 }
-

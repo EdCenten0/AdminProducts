f8876fdd2168fdb3cf95280ee1af69362532543c
Ed Centeno <cchavarriacenteno8@gmail.com>
2023-07-14 16:18:53 -0600

    Categories from api coded

---
+++ js/data/categories.js
@@ -0,0 +1,25 @@
+import { resolve } from "path";
+
+const API = "https://api.escuelajs.co/api/v1";
+
+function fetchData(urlAPI) {
+  return window.fetch(urlAPI);
+}
+
+export async function getCategories() {
+  return new Promise((resolve, reject) => {
+    fetchData(`${API}/categories`)
+      .then((response) => response.json)
+      .then((categories) => resolve(categories))
+      .catch((err) => console.log(err));
+  });
+}
+
+export async function getOneCategories(categoryId) {
+  return new Promise((resolve, reject) => {
+    fetchData(`${API}/categories/${categoryId}`)
+      .then((response) => response.json())
+      .then((product) => resolve(product))
+      .catch((err) => reject(err));
+  });
+}
